35b9ab811e73e81ea37f90b5d2493bdb87984f3a
karismagarduno <93623086+karismagarduno@users.noreply.github.com>
2022-11-07 14:33:03 -0700

    Add files via upload

---
+++ src/Quotes.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+ 
+ 
+const Quotes = () => {
+    
+    const [quote, setQuote] = useState({});
+ 
+    useEffect(() => {
+        getQuote()
+    }, []);
+ 
+    const getQuote = () => {
+ 
+        let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                let dataQuotes = data.quotes;
+                let randomNum = Math.floor(Math.random() * dataQuotes.length);
+                let randomQuote = dataQuotes[randomNum];
+                setQuote(randomQuote);
+                //setAuthor(randomQuote.author);
+            })
+           
+    }
+ 
+    const handleClick= () => {
+        getQuote();
+    }
+   
+    return (
+        <div>
+            <div className= "quote">{quote.quote}</div>
+            <div className= "author">-{quote.author}</div>
+            <button onClick={handleClick}>
+                New Quote
+            </button>
+        </div>
+    )
+}
+
+ 
+export default Quotes;
